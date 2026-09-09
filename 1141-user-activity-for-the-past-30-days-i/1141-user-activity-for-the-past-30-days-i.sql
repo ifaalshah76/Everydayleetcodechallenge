66894c35d04107c714e6dd4f86077bbf820172d7
ifaalshah76 <shahifaal015@gmail.com>
2026-09-09 16:57:10 +0530

    Time: 885 ms (73.16%), Space: 0B (100%) - LeetHub

---
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+SELECT
+    activity_date AS day,
+    COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date >= DATEADD(day, -29, '2019-07-27')
+  AND activity_date <= '2019-07-27'
+GROUP BY activity_date;
