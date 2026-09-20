cf12a04964037831886bc711aef1a4912024aa08
ifaalshah76 <shahifaal015@gmail.com>
2026-09-20 16:59:56 +0530

    Time: 783 ms (96.03%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,12 +1,23 @@
-WITH first_order AS 
-(SELECT customer_id,
-MIN(order_date) AS ist
-FROM delivery
-GROUP BY customer_id)
-
-SELECT 
-ROUND(100.0*SUM(CASE WHEN d.order_date = d.customer_pref_delivery_date THEN 1 ELSE 0 END)/COUNT(*),2) AS immediate_percentage
-FROM first_order f
-JOIN delivery d 
-ON f.customer_id = d.customer_id
-AND f.ist = d.order_date
+WITH first_order AS
+(
+    SELECT
+        *,
+        ROW_NUMBER() OVER(
+            PARTITION BY customer_id
+            ORDER BY order_date
+        ) AS rn
+    FROM Delivery
+)
+SELECT
+    ROUND(
+        100.0 * SUM(
+            CASE
+                WHEN order_date = customer_pref_delivery_date
+                THEN 1
+                ELSE 0
+            END
+        ) / COUNT(*),
+        2
+    ) AS immediate_percentage
+FROM first_order
+WHERE rn = 1;
