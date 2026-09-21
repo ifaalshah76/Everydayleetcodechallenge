40684e63e31367f29d5a02ae8b73025bfb99ffc9
ifaalshah76 <shahifaal015@gmail.com>
2026-09-21 15:48:13 +0530

    Time: 1443 ms (39.72%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,16 +1,18 @@
 WITH firstlog AS
-(SELECT *,
-ROW_NUMBER()OVER(PARTITION BY player_id ORDER BY event_date ) AS Rnk
-FROm activity)
+(SELECT 
+player_id,
+MIN(event_date) AS first_login
+FROM activity
+GROUP BY player_id)
 
 SELECT
     ROUND(
-        1.0 * COUNT(DISTINCT a.player_id) /
+        1.0 * COUNT(DISTINCT f.player_id) /
         (SELECT COUNT(DISTINCT player_id) FROM Activity),
         2
     ) AS fraction
 FROM firstlog f
 JOIN Activity a
     ON f.player_id = a.player_id
-   AND a.event_date = DATEADD(day, 1, f.event_date)
-WHERE f.Rnk = 1;
+   AND a.event_date = DATEADD(day, 1, f.first_login)
+;
